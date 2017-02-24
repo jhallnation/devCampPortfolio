db935df03c2756d8cf5447f1f4a37309325bec71
Jason Hall <hall.jasondaniel@gmail.com>
2017-02-24 14:55:08 -0700

    implemented delete feature on portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -35,8 +35,18 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully removed.' }
+    end
   end
 end
 

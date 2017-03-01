8b96dc8efe5aa5af2bbf33a0a3c171990a87edcf
Jason Hall <hall.jasondaniel@gmail.com>
2017-02-28 20:46:44 -0700

    implented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  get 'angular-items', to: 'portfolios#angular'
+
   get 'about', to: 'pages#about'
 
   get 'contact', to: 'pages#contact'

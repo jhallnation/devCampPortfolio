749c98a6ec542978242b4c069beb0942c0210876
Jason Hall <hall.jasondaniel@gmail.com>
2017-03-13 11:09:55 -0600

    updated portfolio for position control on portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ layout 'portfolio'
   before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular

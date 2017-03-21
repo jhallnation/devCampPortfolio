adf7096d7bfb5c34a9882975f795010b659c276c
Jason Hall <hall.jasondaniel@gmail.com>
2017-03-21 11:51:39 -0600

    fixed action cable call code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_cable.allowed_request_origins = ['https://dv-camp-portfolio.herokuapp.com', 'http://dv-camp-portfolio.herokuapp.com']
-  config.action_cable.url = "wss://dv-camp-portfolio.herokuapp.com"
+  config.action_cable.url = "wss://dv-camp-portfolio.herokuapp.com/cable"
 end

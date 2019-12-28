def427df6444cf5af5310ce7ed33a1165c9cba2b
Jason Hall <jhallnation@gmail.com>
2019-12-28 15:46:00 -0700

    adding some logging for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,9 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::DEBUG
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
